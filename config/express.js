a63afcaf3a21ea9bfbb4c4e5ccab6d9e2c215e44
Valentin Georgiev <valentin.al.georgiev@gmail.com>
2020-05-29 18:41:32 +0300

    feat: project init

---
+++ config/express.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+module.exports = (app) => {
+    
+    //TODO: Setup the view engine
+
+    //TODO: Setup the static files
+
+};
