74d51b60c4a917374e812290be6e756e382ea2a4
xakepa <plamen_ivanov@mail.com>
2020-05-31 19:15:33 +0300

    This fix hide the static files access from the browser

--- config/express.js
+++ config/express.js
@@ -7,8 +7,8 @@ module.exports = (app) => {
   app.engine('.hbs', handlebars({
     extname: '.hbs'
   }))
-  
+
   app.set('view engine', '.hbs');
 
-  app.use('/static', express.static('static'))
+  app.use(express.static('static'))
 };
