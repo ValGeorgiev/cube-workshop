817a89adba2be25bc8a64174fb28ad37a5ba4536
Valentin Georgiev <valentin.al.georgiev@gmail.com>
2020-06-05 18:43:56 +0300

    feat: connect with database

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     development: {
-        port: process.env.PORT || 3000
+        port: process.env.PORT || 3000,
+        databaseUrl: `mongodb+srv://user:${process.env.DB_PASSWORD}@softuni-wabx4.mongodb.net/cubicle?retryWrites=true&w=majority`
     },
     production: {}
 };
